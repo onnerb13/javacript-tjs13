f799503bee1c5a25269040b302eb6de6abaa0bb0
Carlos Brenno <brennosjm@hotmail.com>
2026-01-26 11:23:19 -0300

    Meu primeiro commit

---
+++ helloworld/HelloWorld.js
@@ -0,0 +1,11 @@
+//const leia = require("readline-sync")
+//let mensagem = leia.question("Digite uma mensagem: ")
+
+//console.log(mensagem);
+
+let numero1 = leia.questionInt("digite o primeiro numero: ");
+let numero2 = leia.questionInt("digite o segundo numero: ");
+
+soma = numero1 + numero2;
+
+console.log("A soma deu ", soma);
